e3d963aec2d3cccc38e1a7feb25ff3a23094139d
Eugene Shangin <fessracha@gmail.com>
2020-09-29 00:07:26 +0500

    Resize (#8)
    * WIP, resize
    
    * WIP, resize
    
    * WIP, resize, наконец-то реализовал ресайз
    
    * finish resize
    
    Co-authored-by: Eugene Shangin <evgenii.shangin@ananas-web.ru>

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,25 @@
 import {ExcelComponent} from '@core/ExcelComponent'
 import {createTable} from '@/components/table/table.template'
+import resizeHandler from '@/components/table/table.resize'
+import {shouldResize} from '@/components/table/table.functions';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown']
+    });
+  }
+
+  onMousedown = e => {
+    if (shouldResize(e)) {
+      // eslint-disable-next-line no-invalid-this
+      resizeHandler(this.$root, e)
+    }
+  }
+
   toHTML() {
-    return createTable(20)
+    return createTable(10)
   }
 }
--- src/core/dom.js
+++ src/core/dom.js
@@ -39,9 +39,30 @@ class Dom {
 
     return this
   }
+
+  closest(selector) {
+    return $(this.$el.closest(selector))
+  }
+
+  getCoords() {
+    return this.$el.getBoundingClientRect()
+  }
+
+  findAll(selector) {
+    return this.$el.querySelectorAll(selector)
+  }
+
+  css(styles = {}) {
+    Object
+        .keys(styles)
+        .forEach(key => this.$el.style[key] = styles[key])
+  }
+
+  get data() {
+    return this.$el.dataset
+  }
 }
 
-// event.target
 export function $(selector) {
   return new Dom(selector)
 }
